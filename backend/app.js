ba23991d24916776e8390fbd05a955334dba8350
MSekrst <matija.sekrst13@gmail.com>
2016-11-09 11:07:12 +0100

    Added backend configuration

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import routes from './routes';
+
+const app = express();
+
+// load environment variables from .env file
+dotenv.config();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// use routes defined in an index file
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+});
+
+export default app;
