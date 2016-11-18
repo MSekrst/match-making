645b09ee1d1c26b40f5fba0dffb0580c52fdcfdb
fPolic <frane.polic@fer.hr>
2016-11-18 12:37:02 +0100

    Responsive work

--- src/Table/Company.js
+++ src/Table/Company.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './table.css';
 
 export default class Company extends Component {
-  render() {
-    return <div className="panelRow" key={this.props.id} style={{marginBottom: "3%"}}>
-      <div style={{textAlign: "left", fontSize: "40px", width: "100%"}}>
-        <div style={{display: "inline-block", fontWeight: 400}}>{this.props.name}  </div>
-      </div>
-      <div className="logoBest">
-        <img src={this.props.image} alt="" style={{position: "absolute", height: "60px", maxHeight: "50%", object: "contain"}}/>
-      </div>
-    </div>;
-  }
+    render() {
+        return <div className="panelRow comp" key={this.props.id} style={{ marginBottom: "3%" }}>
+            <div style={{ textAlign: "left", fontSize: "40px", width: "100%" }}>
+                <div className="elipsis-comp" style={{ display: "inline-block", fontWeight: 400 }}>{this.props.name}  </div>
+            </div>
+            <div className="logoBest">
+                <img src={this.props.image} alt="" style={{ position: "absolute", height: "60px", maxHeight: "50%", object: "contain" }} />
+            </div>
+        </div>;
+    }
 }
